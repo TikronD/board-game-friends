b2e42c3bb538b1254b10355cc8ab6c70db62d848
Tikron <d.hahn.gb@gmail.com>
2024-02-19 14:32:08 +0000

    profile login

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
 import CssGrid from "@/component/CssGrid";
+import { auth } from "@clerk/nextjs";
+import { db } from db;
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth();
+
+  const user =
+    await sql`SELECT * FROM profiles where clerk_user_id = ${userId}`;
   return (
-    <div>
-      <CssGrid />
-    </div>
+    <>
+      {userId && user.rowCount === 0 && (
+        <div>
+          <Link href="/profile">
+            <p>Create Profile</p>
+          </Link>
+        </div>
+      )}
+      {userId && user.rowCount !== 0 && (
+        <div>
+          <Link href="/profile">My profile</Link>
+          <CssGrid />
+        </div>
+      )}
+    </>
   );
 }
