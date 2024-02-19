b4367936f4fc3b2c6f5ec977a65e0880de486756
Tikron <d.hahn.gb@gmail.com>
2024-02-19 11:26:07 +0000

    start of team project - structure upload

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h4>&copy; 2024</h4>
+      <h5>
+        Partner Project of <br></br>Ben Shreeve, Edward Stuart, Daniel Hahn and
+        Jimmy
+      </h5>
+    </div>
+  );
+}
