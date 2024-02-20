674e5cf7b728a284610924302616b015a8e8e7fc
Ben <bmshreeve3@gmail.com>
2024-02-20 10:50:53 +0000

    css fixes

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -8,6 +8,7 @@ import bg6 from "@/../public/images/example-images/bg6.jpg";
 import bg7 from "@/../public/images/example-images/bg7.jpg";
 import bg8 from "@/../public/images/example-images/bg8.jpg";
 import bg9 from "@/../public/images/example-images/bg9.jpg";
+
 import "./carousel.css";
 
 export default function Carousel() {
@@ -15,47 +16,47 @@ export default function Carousel() {
     <div className="slider">
       <div className="slide-track">
         <div className="slide">
-          <Image className="image" src={bg1} alt="board-game10" />
+          <Image className="image" src={bg1} alt="board-game1" />
           <h3>Monopoly</h3>
           <p className="price">£20</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg2} alt="board-game11" />
+          <Image className="image" src={bg2} alt="board-game2" />
           <h3>Chess</h3>
           <p className="price">£15</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg3} alt="board-game12" />
+          <Image className="image" src={bg3} alt="board-game3" />
           <h3>Catan</h3>
           <p className="price">£25</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg4} alt="board-game13" />
+          <Image className="image" src={bg4} alt="board-game4" />
           <h3>Small World</h3>
           <p className="price">£30</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg5} alt="board-game14" />
+          <Image className="image" src={bg5} alt="board-game5" />
           <h3>Explorer</h3>
           <p className="price">£40</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg6} alt="board-game15" />
+          <Image className="image" src={bg6} alt="board-game6" />
           <h3>Agricola</h3>
           <p className="price">£50</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg7} alt="board-game16" />
+          <Image className="image" src={bg7} alt="board-game7" />
           <h3>Stardew Valley</h3>
           <p className="price">£27</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg8} alt="board-game17" />
+          <Image className="image" src={bg8} alt="board-game8" />
           <h3>Orb</h3>
           <p className="price">£15</p>
         </div>
         <div className="slide">
-          <Image className="image" src={bg9} alt="board-game18" />
+          <Image className="image" src={bg9} alt="board-game9" />
           <h3>Collection</h3>
           <p className="price">£15</p>
         </div>
