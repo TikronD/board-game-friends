7930a5980149b24ad50449a5a42234cf0dc34de1
Tikron <d.hahn.gb@gmail.com>
2024-02-20 15:37:16 +0000

    header/footer changes

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,13 +1,18 @@
-import "./footer.css";
+// import "./footer.css";
+import "./header.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <h4>&copy; 2024</h4>
-      <h5>
-        Partner Project of <br></br>Ben Shreeve, Edward Stuart, Daniel Hahn and
-        Jimmy Kerr
-      </h5>
-    </div>
+    <section>
+      <div className="container">
+        <div className="imageheader">
+          <h4>&copy; 2024</h4>
+          <h5>
+            Partner Project of <br></br>Ben Shreeve, Edward Stuart, Daniel Hahn
+            and Jimmy Kerr
+          </h5>
+        </div>
+      </div>
+    </section>
   );
 }
