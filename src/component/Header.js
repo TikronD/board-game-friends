e80e758dd31174f17a878b247b68248ffa9800ed
Tikron <d.hahn.gb@gmail.com>
2024-02-21 10:32:06 +0000

    header/footer

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,20 +1,24 @@
+Header.js;
 import "./header.css";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/../public/images/Logo-removebg.png";
+// import Wave from "@/component/Wave";
 
 export default function Header() {
   return (
+    // <section className="imageheader">
     <section>
-      <div className="container">
-        <div className="imageheader">
-          <h1>Board Game Friends</h1>
-          <nav>
-            <Link href="/">Home</Link>
-            <Link href="./collection">My Collection</Link>
-            <Link href="./events">Events</Link>
-            <Link href="/marketplace">Marketplace</Link>
-            <Link href="./about">About</Link>
-          </nav>
-        </div>
+      <div className="headersection">
+        <Image className="logo" src={logo} alt="boardgame friends logo" />
+        <h1>Board Game Friends</h1>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="./collection">My Collection</Link>
+          <Link href="./events">Events</Link>
+          <Link href="/marketplace">Marketplace</Link>
+          <Link href="./about">About</Link>
+        </nav>
       </div>
     </section>
   );
