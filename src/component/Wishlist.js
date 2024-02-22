5e75ff55cb122d119cb03c1a21f0bc9d95f7f720
James <jimmykerr2003@gmail.com>
2024-02-22 16:44:56 +0000

    fix errors

--- src/component/Wishlist.js
+++ src/component/Wishlist.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { db } from "@/lib/db";
 import "./wishlist.css";
 import { auth } from "@clerk/nextjs";
@@ -6,104 +7,119 @@ import apiCall, { apiBoardGame, apiSearchStrict } from "@/lib/apiCall";
 import WishlistForm from "./WishlistForm.jsx";
 
 export default async function Wishlist() {
-  const api = await apiCall();
-  const now = new Date();
-  const timestamp = now.getTime();
-  const { userId } = auth();
+    const api = await apiCall();
+    const now = new Date();
+    const timestamp = now.getTime();
+    const { userId } = auth();
 
-  const idQuery = await db.query(
-    `SELECT id from profiles WHERE clerk_user_id = $1`,
-    [userId]
-  );
-
-  const id = idQuery.rows[0].id;
-
-  const marketplaceData = await db.query(
-    `SELECT game_title, price from marketplace`
-  );
-
-  const wishlistData = await db.query(
-    `SELECT game_title, price_max, condition_min, extras, api_id from wishlist WHERE user_id = ${id}`
-  );
-
-  const matches = marketplaceData.rows.filter((game) =>
-    wishlistData.rows.some(
-      (wish) =>
-        wish.game_title === game.game_title && game.price <= wish.price_max
-    )
-  );
-
-  async function handleWishList(formData) {
-    "use server";
-
-    const gameTitle = formData.get("game_title");
-    const priceMax = formData.get("price_max");
-    const conditionMin = formData.get("condition_min");
-    const extras = formData.get("extras");
-    const search = gameTitle.replace(" ", "+");
-    const data = await apiSearchStrict(search);
-
-    await db.query(
-      `INSERT INTO wishlist (user_id, game_title, price_max, condition_min, extras, time, api_id) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-      [id, gameTitle, priceMax, conditionMin, extras, timestamp, data[0].$.id]
+    const idQuery = await db.query(
+        `SELECT id from profiles WHERE clerk_user_id = $1`,
+        [userId]
     );
 
-    revalidatePath("/wishlist/add");
-  }
+    const id = idQuery.rows[0].id;
 
-  return (
-    <div className="wishlist-container">
-      <div className="format">
-        <h2 className="wishlist-title">Add Game to WishList</h2>
-        <WishlistForm handleWishList={handleWishList} api={api} />
-      </div>
-      <div className="format">
-        <h2 className="wishlist-title">Your Wishlist</h2>
-        <div className="your-wishlist-container">
-          {wishlistData.rows.map(async (game) => {
-            const boardGameData = await apiBoardGame(game.api_id);
-            return (
-              <div>
-                <div
-                  className="your-wishlist"
-                  key={game.game_title + game.price_max}
-                >
-                  <img
-                    className="wishlist-img"
-                    src={boardGameData[0].image[0]}
-                  />
-                  <h3 className="wishlist-game">
-                    <strong>{game.game_title}</strong>
-                  </h3>
-                  <p className="wishlist-attributes">
-                    Max Price: £{game.price_max}
-                  </p>
-                  <p className="wishlist-attributes">
-                    Condition: {game.condition_min}
-                  </p>
-                  <p className="wishlist-attributes">Extras: {game.extras}</p>
+    const marketplaceData = await db.query(
+        `SELECT game_title, price from marketplace`
+    );
+
+    const wishlistData = await db.query(
+        `SELECT id game_title, price_max, condition_min, extras, api_id from wishlist WHERE user_id = ${id}`
+    );
+
+    const matches = marketplaceData.rows.filter((game) =>
+        wishlistData.rows.some(
+            (wish) =>
+                wish.game_title === game.game_title &&
+                game.price <= wish.price_max
+        )
+    );
+
+    async function handleWishList(formData) {
+        "use server";
+
+        const gameTitle = formData.get("game_title");
+        const priceMax = formData.get("price_max");
+        const conditionMin = formData.get("condition_min");
+        const extras = formData.get("extras");
+        const search = gameTitle.replace(" ", "+");
+        const data = await apiSearchStrict(search);
+
+        await db.query(
+            `INSERT INTO wishlist (user_id, game_title, price_max, condition_min, extras, time, api_id) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+            [
+                id,
+                gameTitle,
+                priceMax,
+                conditionMin,
+                extras,
+                timestamp,
+                data[0].$.id,
+            ]
+        );
+
+        revalidatePath("/wishlist/add");
+    }
+
+    return (
+        <div className="wishlist-container">
+            <div className="format">
+                <h2 className="wishlist-title">Add Game to WishList</h2>
+                <WishlistForm handleWishList={handleWishList} api={api} />
+            </div>
+            <div className="format">
+                <h2 className="wishlist-title">Your Wishlist</h2>
+                <div className="your-wishlist-container">
+                    {wishlistData.rows.map(async (game) => {
+                        const boardGameData = await apiBoardGame(game.api_id);
+                        return (
+                            <div key={id}>
+                                <div
+                                    className="your-wishlist"
+                                    key={game.game_title + game.price_max}
+                                >
+                                    <img
+                                        alt="Boardgame Image"
+                                        className="wishlist-img"
+                                        src={boardGameData.image[0]}
+                                    />
+                                    <h3 className="wishlist-game">
+                                        <strong>{game.game_title}</strong>
+                                    </h3>
+                                    <p className="wishlist-attributes">
+                                        Max Price: £{game.price_max}
+                                    </p>
+                                    <p className="wishlist-attributes">
+                                        Condition: {game.condition_min}
+                                    </p>
+                                    <p className="wishlist-attributes">
+                                        Extras: {game.extras}
+                                    </p>
+                                </div>
+                                <hr className="h-px my-2 bg-gray-300 border-0 dark:bg-gray-700"></hr>
+                            </div>
+                        );
+                    })}
                 </div>
-                <hr className="h-px my-2 bg-gray-300 border-0 dark:bg-gray-700"></hr>
-              </div>
-            );
-          })}
+            </div>
+            <div className="format">
+                <h2 className="wishlist-title">Marketplace Matches</h2>
+                <div className="your-wishlist-container match-container">
+                    {matches.map((game) => {
+                        return (
+                            <div
+                                className="matches"
+                                key={game.game_title + game.price}
+                            >
+                                <h3>
+                                    <strong>{game.game_title}</strong>
+                                </h3>
+                                <p>Price: £{game.price}</p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="format">
-        <h2 className="wishlist-title">Marketplace Matches</h2>
-        <div className="your-wishlist-container match-container">
-          {matches.map((game) => {
-            return (
-              <div className="matches" key={game.game_title + game.price}>
-                <h3>
-                  <strong>{game.game_title}</strong>
-                </h3>
-                <p>Price: £{game.price}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
